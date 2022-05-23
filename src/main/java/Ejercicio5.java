52913b9ea6c26694dfc3e9b60849638f270bdbe7
Luis Mi <luismiguelbravolopez3@gmail.com>
2022-05-23 19:19:34 +0200

    Comit de nuevo

--- src/main/java/Ejercicio5.java
+++ src/main/java/Ejercicio5.java
@@ -8,6 +8,14 @@ public class Ejercicio5 {
         personajes = new Personajes(ObtenerPersonajesRequest.Companion.get());
     }
 
+
+    public static void main(String[] args) {
+        Ejercicio5 ej1 = new Ejercicio5();
+        System.out.println(ej1.TieneImagen());
+    }
+
+
+
     // TODO Haz una función que te devuelva un array compuesto por:
     //  Personajes que tienen imagen asociada.
 
@@ -17,14 +25,15 @@ public class Ejercicio5 {
         Personaje[] out = personajes.obtenerTodos();
 
         int i = 0;
-
+        int a=0;
         for(i=0; i < out.length; i++){
-            if(out[i].getImage() != null){
+            if(!out[i].getImage().isEmpty()){
                 out[i]=out[i];
+                a++;
             }
         }
 
-        return i;
+        return a;
     }
 
 
